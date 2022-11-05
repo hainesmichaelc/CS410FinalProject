4dbef57696e358c68d4a4bbcee3e75d8c1905e63
Kevin Eveker <kevin.eveker@c3.ai>
2022-11-05 19:23:14 -0400

    added return of top 10 google results

--- scripts/app/app.py
+++ scripts/app/app.py
@@ -1,13 +1,10 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from googlesearch import search
 
 app = Flask(__name__)
 
-
-# @app.route('/')
-# def index():
-#     return 'Web App with Python Flask!'
 
 @app.route('/')
 @app.route('/index')
@@ -19,8 +16,15 @@ def googlesearchresult():
     if request.method == 'POST':
         # print(request)
         google_search_string = request.form['searchstring']
+        search_result = search(google_search_string, num=10, stop=10)
+        search_result_string = ""
+        for item in search_result:
+            search_result_string += "<div>"
+            search_result_string +=item
+            search_result_string += "</div>"
+            search_result_string +="<br>"
         # print(google_search_string)
-        return render_template('googlesearchresult.html',searchstring=google_search_string)
+        return render_template('googlesearchresult.html',searchstring=google_search_string, searchresult=search_result_string)
 
 
 app.run(host='0.0.0.0', port=81)
