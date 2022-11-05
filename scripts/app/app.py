ff328c78a977026dbbf79aea1e4bacb85a26dd0a
Kevin Eveker <kevin.eveker@c3.ai>
2022-11-05 17:39:32 -0400

    starting point for flask

---
+++ scripts/app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+# @app.route('/')
+# def index():
+#     return 'Web App with Python Flask!'
+
+@app.route('/')
+@app.route('/index')
+def index():
+    name = 'Rosalia'
+    return render_template('index.html', title='Welcome', username=name)
+
+app.run(host='0.0.0.0', port=81)
