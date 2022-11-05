924a656e834fbc061d1f64926784aa298ec64ea0
Kevin Eveker <kevin.eveker@c3.ai>
2022-11-05 18:45:20 -0400

    got callback from search page working

--- scripts/app/app.py
+++ scripts/app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -12,6 +13,15 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     name = 'Rosalia'
-    return render_template('index.html', title='Welcome', username=name)
+    return render_template('index.html')
+
+@app.route('/googlesearchresult',methods = ['POST', 'GET'])
+def googlesearchresult():
+    if request.method == 'POST':
+        # print(request)
+        google_search_string = request.form['searchstring']
+        # print(google_search_string)
+        return render_template('googlesearchresult.html',searchstring=google_search_string)
+
 
 app.run(host='0.0.0.0', port=81)
